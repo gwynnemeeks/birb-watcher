4b51867fd771698ef1dcb07c561d8ba7bc749b98
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-08-08 12:51:18 -0500

    this is the third success

---
+++ src/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Scat-Surprise</h2>
+        <button className="btn btn-secondary">
+        <i class="fas fa-poo"></i>
+          </button>
+      </div>
+    );
+  }
+}
+
+export default App;
