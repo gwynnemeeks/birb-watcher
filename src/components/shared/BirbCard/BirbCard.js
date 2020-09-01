f60d1b400cba671b826a54314b04e6e85d0ca5d3
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-08-31 19:44:45 -0500

    technically reading bird cards

---
+++ src/components/shared/BirbCard/BirbCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import birbShape from '../../../helpers/propz/birbShape';
+
+class BirbCard extends React.Component {
+  static propTypes = {
+    birb: birbShape.birbShape,
+  }
+
+  render() {
+    const { birb } = this.props;
+
+    return (
+      <div className="Bird card">
+          <div className="card-columns">
+        <div className="card-body">
+          <h5 className="card-title">{birb.type}</h5>
+          <p className="card-text">{birb.notes}</p>
+        </div>
+      </div>
+      </div>
+    );
+  }
+}
+
+export default BirbCard;
